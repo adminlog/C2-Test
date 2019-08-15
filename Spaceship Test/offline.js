6eefabd579ddb1bde80aa97d5e2444264991b301
Anthony <anthonyreimche@gmail.com>
2019-08-15 15:57:14 +0800

    Add files via upload

---
+++ Spaceship Test/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1565855673,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/playerbulleta-sheet0.png",
+		"images/playerbulletb-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/enemy-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/hiteffect-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/explosion-sheet1.png",
+		"images/hiteffect1-sheet0.png",
+		"images/enemy1-sheet0.png",
+		"images/enemy1-sheet1.png",
+		"images/enemy2-sheet0.png",
+		"images/enemy2-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}

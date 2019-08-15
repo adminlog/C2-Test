b095f6b1f6b9c0ce84ac1eec9bf7816c0114fa59
Anthony <anthonyreimche@gmail.com>
2019-08-15 16:01:57 +0800

    Add files via upload

---
+++ RPS Test/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1565855868,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bgui-sheet0.png",
+		"images/炎姬ui-sheet0.png",
+		"images/骨女ui-sheet0.png",
+		"images/dialogueui-sheet0.png",
+		"images/dialogueinteract-sheet0.png",
+		"images/buttonui-sheet0.png",
+		"images/fadeui-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
